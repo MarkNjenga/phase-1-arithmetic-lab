9415439c540cb6dca2fa660ee4ccdbb2ee72dc5b
Mark Njenga <marknjenga25@gmail.com>
2024-06-25 13:36:57 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100);
+
+let num3 = 13;
+let num4 = 4;
+const mod = num4 % num3;
+let max = Math.max(2,4,6,20);
